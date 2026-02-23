26a68fc1d0ff0f046ba03045c9cfe228e90ac6d6
PYticPH <pyticph@gmail.com>
2026-02-23 17:04:53 +0100

    chore: Add js func to fetch color schemes

--- script.js
+++ script.js
@@ -3,10 +3,35 @@
 const getColorSchemeBtn = document.getElementById('get-color-scheme')
 const colorPicker = document.getElementById('color-picker')
 
-let colorSeed = ''
+let colorSeed = '#000000'
 
 colorPicker.addEventListener('change', (e) => colorSeed = e.target.value)
 
 getColorSchemeBtn.addEventListener('click', () => {
   const schemeMode = document.getElementById('scheme-mode').value
+
+  fetch(`https://www.thecolorapi.com/scheme?hex=${colorSeed.slice(1)}&mode=${schemeMode}&count=6`)
+    .then(res => res.json())
+    .then(data => generateColorPallet(data.colors))
 })
+
+function generateColorPallet(colorSchemeArr) {
+
+  const colorPalette = document.getElementById('color-palette')
+
+  colorPalette.innerHTML = ''
+
+  const divs = colorSchemeArr.map(
+    ({ hex }) =>
+      `
+      <div>
+        <button class="color-code">${hex.value}</button>
+      </div>
+    `
+  ).join('')
+
+  colorPalette.innerHTML += divs
+
+  document.querySelectorAll('#color-palette div')
+    .forEach((element, index) => element.style.backgroundColor = colorSchemeArr[index].hex.value)
+}
